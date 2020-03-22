c15142cd40e5805ada93dd400d853f072ad12999
CARLOS FELIPE OQUENDO ESCOBAR <carlosoquendo2@hotmail.com>
2020-03-22 17:18:19 -0500

    Add tournament entities

--- Soccer.Web/Data/DataContext.cs
+++ Soccer.Web/Data/DataContext.cs
@@ -8,7 +8,15 @@ namespace Soccer.Web.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
+        public DbSet<GroupDetailEntity> GroupDetails { get; set; }
+
+        public DbSet<GroupEntity> Groups { get; set; }
+
+        public DbSet<MatchEntity> Matches { get; set; }
 
         public DbSet<TeamEntity> Teams { get; set; }
+
+        public DbSet<TournamentEntity> Tournaments { get; set; }
+
     }
 }
