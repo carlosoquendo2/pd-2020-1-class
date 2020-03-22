eea9b15e1297ff7ddb0ab15c6236a4f24ad5c485
CARLOS FELIPE OQUENDO ESCOBAR <carlosoquendo2@hotmail.com>
2020-03-22 17:39:04 -0500

    Add index to tema entity

--- Soccer.Web/Controllers/TeamsController.cs
+++ Soccer.Web/Controllers/TeamsController.cs
@@ -43,24 +43,34 @@ namespace Soccer.Web.Controllers
             return View(teamEntity);
         }
 
-        // GET: Teams/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Teams/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,LogoPath")] TeamEntity teamEntity)
+        public async Task<IActionResult> Create(TeamEntity teamEntity)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(teamEntity);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    if (ex.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, $"Already exist the team: {teamEntity.Name}");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, ex.InnerException.Message);
+                    }
+                }
             }
             return View(teamEntity);
         }
@@ -81,12 +91,9 @@ namespace Soccer.Web.Controllers
             return View(teamEntity);
         }
 
-        // POST: Teams/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,LogoPath")] TeamEntity teamEntity)
+        public async Task<IActionResult> Edit(int id, TeamEntity teamEntity)
         {
             if (id != teamEntity.Id)
             {
@@ -95,23 +102,24 @@ namespace Soccer.Web.Controllers
 
             if (ModelState.IsValid)
             {
+                _context.Update(teamEntity);
+
                 try
                 {
-                    _context.Update(teamEntity);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (Exception ex)
                 {
-                    if (!TeamEntityExists(teamEntity.Id))
+                    if (ex.InnerException.Message.Contains("duplicate"))
                     {
-                        return NotFound();
+                        ModelState.AddModelError(string.Empty, $"Already exist the team: {teamEntity.Name}");
                     }
                     else
                     {
-                        throw;
+                        ModelState.AddModelError(string.Empty, ex.InnerException.Message);
                     }
                 }
-                return RedirectToAction(nameof(Index));
             }
             return View(teamEntity);
         }
